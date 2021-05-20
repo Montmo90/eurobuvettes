9571332e21042faf5032581e2b68ed57f4de9ba6
Jersee <jeremy.chekrounn@gmail.com>
2021-05-20 21:21:26 +0200

    patch admin

--- www/src/prive.js
+++ www/src/prive.js
@@ -2,7 +2,7 @@ function LogAdmin (){
     var pass=document.getElementById("mdp");
     console.log(pass.value);
     if(pass.value==MDP){
-    document.querySelector("form").innerHTML="<p>Test</p>";
+    document.querySelector("form").innerHTML="<p>Vous êtes désormais connecté en tant qu'administrateur.</p><br><div>Ajouter un volontaire</div><input type=textfield><input type=button value=Confirmer><br><div>Programmer une buvette</div><input type=textfield><input type=button value=Confirmer><br><div>Programmer un match</div><input type=textfield><input type=button value=Confirmer>";
     }
     else {
         alert("Mauvais mot de passe !");
