15dee736475672551a85d5504e01b0061b9ee82b
Jersee <jeremy.chekrounn@gmail.com>
2021-05-27 17:10:24 +0200

    Bouton Affectations

---
+++ www/src/affectations.js
@@ -0,0 +1,68 @@
+function AfficheFormVolontaire(){
+    //var choix=document.getElementById("mdp");
+    //console.log(choixVol);
+    document.getElementById("choixVol").style.display="initial";
+    document.getElementById("choixBuv").style.display="none";
+    /*document.getElementById("choixVol").innerHTML=`<form>
+    <fieldset class="panel">
+        <legend>Affecter un volontaire :</legend><br>
+            Match :
+                <input type = "text" list = "match_list" name="match"> 
+                    <datalist id = "match_list">
+                        <option value = "Angleterre - Russie">
+                        <option value = "Irelande - Espagne">
+                        <option value = "France - Croatie">
+                    </datalist><br/><br/>
+            Buvette : 
+                <input type = "text" list = "buvette_list" name="buvettes"> 
+                    <datalist id = "buvette_list">
+                        <option value = "En-Ru id-1">
+                        <option value = "Ir-Es id-7">
+                        <option value = "Fr-Cr id-3">
+                    </datalist><br/><br/>
+            Volontaire : 
+                <input type = "text" list = "volontaire_list" name="volontaire"> 
+                    <datalist id = "volontaire_list">
+                        <option value = "Lorem Ipsum">
+                        <option value = "Le chat">
+                        <option value = "isxelA">
+                    </datalist><br/><br/>
+            Responsable :
+                <input type="checkbox"><br><br>
+                <input type="submit"> - <input type="reset"><br><br>
+    </fieldset>
+</form>`;
+*/
+}
+
+function AfficheFormBuvette(){
+    //var choix=document.getElementById("mdp");
+    //console.log(choixBuv);
+    document.getElementById("choixVol").style.display="none";
+    document.getElementById("choixBuv").style.display="initial";
+    /*document.getElementById("choixBuv").innerHTML=`<form>
+    <fieldset class="panel">
+        <legend>Ouvrir une buvette :</legend><br>
+            Match :
+                <input type = "text" list = "match_list" name="match"> 
+                    <datalist id = "match_list2">
+                        <option value = "Angleterre - Russie">
+                        <option value = "Irelande - Espagne">
+                        <option value = "France - Croatie">
+                    </datalist><br/><br/>
+            Buvette : 
+                <input type = "text" list = "buvette_list" name="buvettes"> 
+                    <datalist id = "buvette_list2">
+                        <option value = "En-Ru id-1">
+                        <option value = "Ir-Es id-7">
+                        <option value = "Fr-Cr id-3">
+                    </datalist><br/><br/>
+            Ouvert :
+                <input type="checkbox"><br><br>
+                <input type="submit"> - <input type="reset"><br><br>
+    </fieldset>
+</form>
+</div>`;
+*/
+
+}
