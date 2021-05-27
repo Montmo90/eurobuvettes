500acf076d9379e9f166bd243af6c90caf827326
Montmo90 <72647113+Montmo90@users.noreply.github.com>
2021-05-27 17:35:39 +0200

    Change mise en page Affectation et modification des input css

--- www/src/affectations.js
+++ www/src/affectations.js
@@ -3,36 +3,10 @@ function AfficheFormVolontaire(){
     //console.log(choixVol);
     document.getElementById("choixVol").style.display="initial";
     document.getElementById("choixBuv").style.display="none";
-    /*document.getElementById("choixVol").innerHTML=`<form>
-    <fieldset class="panel">
-        <legend>Affecter un volontaire :</legend><br>
-            Match :
-                <input type = "text" list = "match_list" name="match"> 
-                    <datalist id = "match_list">
-                        <option value = "Angleterre - Russie">
-                        <option value = "Irelande - Espagne">
-                        <option value = "France - Croatie">
-                    </datalist><br/><br/>
-            Buvette : 
-                <input type = "text" list = "buvette_list" name="buvettes"> 
-                    <datalist id = "buvette_list">
-                        <option value = "En-Ru id-1">
-                        <option value = "Ir-Es id-7">
-                        <option value = "Fr-Cr id-3">
-                    </datalist><br/><br/>
-            Volontaire : 
-                <input type = "text" list = "volontaire_list" name="volontaire"> 
-                    <datalist id = "volontaire_list">
-                        <option value = "Lorem Ipsum">
-                        <option value = "Le chat">
-                        <option value = "isxelA">
-                    </datalist><br/><br/>
-            Responsable :
-                <input type="checkbox"><br><br>
-                <input type="submit"> - <input type="reset"><br><br>
-    </fieldset>
-</form>`;
-*/
+
+    document.getElementById("btnAffecter").disabled = true;
+    document.getElementById("btnOuvrir").disabled = false;
+    
 }
 
 function AfficheFormBuvette(){
@@ -40,29 +14,8 @@ function AfficheFormBuvette(){
     //console.log(choixBuv);
     document.getElementById("choixVol").style.display="none";
     document.getElementById("choixBuv").style.display="initial";
-    /*document.getElementById("choixBuv").innerHTML=`<form>
-    <fieldset class="panel">
-        <legend>Ouvrir une buvette :</legend><br>
-            Match :
-                <input type = "text" list = "match_list" name="match"> 
-                    <datalist id = "match_list2">
-                        <option value = "Angleterre - Russie">
-                        <option value = "Irelande - Espagne">
-                        <option value = "France - Croatie">
-                    </datalist><br/><br/>
-            Buvette : 
-                <input type = "text" list = "buvette_list" name="buvettes"> 
-                    <datalist id = "buvette_list2">
-                        <option value = "En-Ru id-1">
-                        <option value = "Ir-Es id-7">
-                        <option value = "Fr-Cr id-3">
-                    </datalist><br/><br/>
-            Ouvert :
-                <input type="checkbox"><br><br>
-                <input type="submit"> - <input type="reset"><br><br>
-    </fieldset>
-</form>
-</div>`;
-*/
+
+    document.getElementById("btnAffecter").disabled = false;
+    document.getElementById("btnOuvrir").disabled = true;
 
 }
