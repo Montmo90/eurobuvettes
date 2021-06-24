c374ca1f74cf8e362d1cae0347b3892966978639
Morgan <tranquard.morgan@gmail.com>
2021-06-24 14:06:16 +0200

    Passage faux-mvc

---
+++ mvc/app/statistiques/statistiques.js
@@ -0,0 +1,167 @@
+function dbg(x){
+    console.log(x);
+}
+
+// Selectionne 
+function userInput(usrInput){
+    var input=document.querySelector(usrInput);
+    return input;
+}
+
+// reinitialise les champs du formulaire
+function uncheck() {
+    userInput("#TopVol").checked = false;
+    userInput("#TopBuv").checked = false;
+    userInput("#matchs").value = "";
+}
+
+function getStats() {
+    var stats = document.querySelector(".stats");
+    stats.style = "visibility: visible";
+    // input dans les variables
+    var topVol = userInput("#TopVol").checked;
+    var topBuv = userInput("#TopBuv").checked;
+    var match = userInput("#matchs").value;
+    var headerD = "<div class=\"resultat-membres-header\"><span>";
+    var headerF = "</span></div>";
+    var divContent = "";
+    var value = "";
+    // Si input vide, renvoyez pas de resultat
+    if (topVol == "" && topBuv == "" && match == "")
+        value = "Pas de résultat !";   
+    else
+        if (topVol == true && topBuv == false && !match) {
+            divContent = topVolontaires();
+            value = "Top Volontaires";
+        }
+    else
+        if (topBuv == true && topVol == false && !match) {
+            divContent = topBuvettes();
+            value = "Top Buvettes";
+        }
+    else
+        if (match && topVol == false && topBuv == false) {
+            value = "Match (" + match + ") :";
+            divContent = currentMatch(match);
+        }
+    else {
+        value = "Trop d'arguments !";
+    }
+    stats.innerHTML = headerD + value + headerF + divContent;
+    // reinitialise le champ du formulaire
+    uncheck();
+    
+    
+}
+
+function topVolontaires () {
+    var content = "";
+    // Tri la liste d'objet volontaire du plus grand au plus petit
+    // La fonction à l'interieur sers à comparer les proprietés de l'objet
+    // afin de pouvoir trier l'objet grace à la methode sort(), prévue de base pour les chaines de caractères.
+    listeVolontaires.sort(function compare(a, b){
+        if (a.nbPart > b.nbPart)
+           return -1;
+        if (a.nbPart < b.nbPart )
+           return 1;
+        return 0;
+    });
+    // Affiche le top 5 des volontaires
+    for (var i = 0; i < 5; i++) {
+        topVolName = listeVolontaires[i].nom;
+        topVolNbPart = listeVolontaires[i].nbPart;
+
+        content +=`<div class="resultat-membres-item center">
+                    <span>
+                        ${topVolName}
+                    </span>
+                    <span>
+                        ${topVolNbPart} participation(s)
+                    </span>
+                    </div>`;
+    }
+    return content;
+
+}
+
+function topBuvettes () {
+    var content = "";
+    // Tri la liste d'objet volontaire du plus grand au plus petit
+    // La fonction à l'interieur sers à comparer les proprietés de l'objet
+    // afin de pouvoir trier l'objet grace à la methode sort(), prévue de base pour les chaines de caractères.
+    listeBuvettes.sort(function compare(a, b){
+        if (a.nbVol > b.nbVol)
+           return -1;
+        if (a.nbVol < b.nbVol )
+           return 1;
+        return 0;
+    });
+    // Affiche le top 5 des volontaires
+    for (var i = 0; i < 5; i++) {
+        topBuvName = listeBuvettes[i].nom;
+        topBuvPlace = listeBuvettes[i].lieu;
+
+        content += `<div class="resultat-membres-item center">
+                        <span>
+                            ${topBuvName}
+                        </span>
+                        <span>
+                            ${topBuvPlace}
+                        </span>
+                    </div>`;
+    }
+    return content;
+
+}
+
+function currentMatch(match) {
+    var content = "";
+    var infosMatch;
+    var InfosBuvettes;
+    // parcours la liste des matchs jusqu'a trouver la date correspondante
+    // puis enregistre la reference à l'objet dans la variable id
+    for (var i = 0; i < listeMatch.length; i++) {
+        if (listeMatch[i].date == match)
+            var id = listeMatch[i];
+    }
+        infosFlag = `<div>
+                        <div>
+                            <div class="statsMatch">
+                                <div>
+                                    <img width"80" height="60" src="${id.teamA.drapeau}">
+                                </div>
+                                
+                                <div>
+                                </div>
+                                <div>
+                                <img width="80" height="60" src="${id.teamB.drapeau}">
+                                </div>
+                    </div>`;
+        
+        infosMatch = `<div class="center statsMatch">
+                        
+                        <div>${id.teamA.pays}</div>
+                        
+                        <div>${id.score}</div>
+                        
+                        <div>${id.teamB.pays}</div>
+                        
+                        </div>
+                    `;  
+        InfosBuvettes = `<div class="center statsMatch">
+                            
+                            <div>Buvette : ${id.buvette.nom}</div>
+                            
+                            <div>Lieu : ${id.buvette.lieu}</div>
+                            
+                            <div>Responsable : ${id.buvette.resp}</div>
+                        </div>
+                        `;
+        content += infosFlag + infosMatch + InfosBuvettes;
+    return content;
+}
+
+function listStatMatch(){
+    for(var j = listeMatch.length - 1 ; j >= 0 ; j-- )
+    document.getElementById("listeMatchs").innerHTML += `<option value="${listeMatch[j].date}">${listeMatch[j].teamA.pays} - ${listeMatch[j].teamB.pays}</option>`;
+}
---
+++ mvc/src/bdd.js
@@ -0,0 +1,92 @@
+const MDP = "admin123";
+
+/*Création du constructeur Volontaire*/
+function Volontaire (nom, age, nbPart, resp) {
+    this.nom = nom;
+    this.age = age;
+    this.nbPart = nbPart;
+    this.resp = resp;
+}
+
+/*Création d'un tableau vide*/
+var listeVolontaires = [];
+
+/*Création des listes pour chaque propriétées du constructeur Volontaire*/
+/*Split une chaine de charactére en tableau avec en paramétre une |*/
+var listeNom = "Fabian|Morgan|Jeremy|Killian|Cédric|Fairouze|Jean-Michel|Alexis M|Alexis T|Kevin|Aziz".split("|");
+var listeAge = "30|30|24|22|31|#On-demande-pas-l-age-d-une-dame|16|27|22|31|?".split("|");
+var listeNbPart = "13|7|11|42|2|31|22|8|12|2|5".split("|");
+var listeResp = "false|true|false|true|true|false|false|false|true|false|true".split("|");
+
+/*Grace à une boucle j'ajoute mes listes à mon tableau listeVolontaires*/
+for (var li = 0 ; li < listeNom.length; li++) {
+    /*Ajoute un nouveau Objet Volontaire avec comme paramétre la valeur du tableau à l'indice li*/
+    listeVolontaires.push(new Volontaire(listeNom[li], listeAge[li], parseInt(listeNbPart[li]), (listeResp[li] == "true")))
+}
+
+//dbg(listeVolontaires[6].nom);//Jean-Michel
+
+function Equipe (id, pays, drapeau) {
+    this.id = id;
+    this.pays = pays;
+    this.drapeau = drapeau;
+}
+
+var listeEquipe = [new Equipe("fr", "FRANCE", "img/fr.webp"),
+                   new Equipe("alb", "ALBANIE", "img/alb.webp"),
+                   new Equipe("ga", "GALLES", "img/ga.webp"),
+                   new Equipe("an", "ANGLETERRE", "img/gb.webp"),
+                   new Equipe("tu", "TURQUIE", "img/tr.webp"),
+                   new Equipe("po", "PORTUGAL", "img/pt.webp"),
+                   new Equipe("al", "ALLEMAGNE", "img/de.webp"),
+                   new Equipe("es", "ESPAGNE", "img/es.webp"),
+                   new Equipe("ro", "ROUMANIE", "img/ro.webp"),
+                   new Equipe("su", "SUEDE", "img/se.webp"),
+                   new Equipe("sl", "SLOVENIE", "img/sl.webp"),
+                   new Equipe("ru", "RUSSIE", "img/ru.webp"),
+                   new Equipe("cr", "CROATIE", "img/cr.webp"),
+                   new Equipe("ir", "IRLANDE", "img/ir.webp"),
+                   new Equipe("uk", "UKRAINE", "img/ua.webp"),
+                   new Equipe("rt", "REPUBLIQUE TCHEQUE", "img/cz.webp")];
+
+/*Création du constructeur Buvette*/
+function Buvette(ouvert, nom, lieu, nbVol, listVol, resp) {
+    this.ouvert = ouvert;
+    this.nom = nom;
+    this.lieu = lieu;
+    this.nbVol = nbVol;
+    this.listVol = listVol;
+    this.resp = resp;
+}
+
+var listeBuvettes = [new Buvette(true, "Drinkons ensemble!","Parc des princes", 4, "Jean-Michel,Kevin,Fairouze,Jeremy", "Jean-Michel"), 
+                     new Buvette(true, "ALCOLO","Stade de France", 1, "Alexis M", "Alexis M"),
+                     new Buvette(false, "Yapado","Stade Vélodrome", 5, "Fabian,Morgan,Jeremy,Killian,Cédric", "Killian"),
+                     new Buvette(false, "Bubbletohol","Allianz Riviera", 3, "Fairouze,Aziz,Alexis M", "Fairouze"),
+                     new Buvette(false,"Balon bourré","Stade Geoffroy-Guichard", 2, "Aziz,Jeremy", "Jeremy"),
+                     new Buvette(true, "Open Bar","Stadium", 1, "Jean-Michel", "Jean-Michel"),
+                     new Buvette(false, "Un chat un verre","Stade Bollaert", 2, "Cédric,Jeremy", "Cédric"),
+                     new Buvette(true, "Débit de boisson","Stade Pierre Mauroy", 3, "Alexis M,Killian,Morgan", "Morgan"),
+                     new Buvette(false, "Glouglou","Stade des lumières", 4, "Fabian,Jean-Michel,Jeremy,Kevin", "Kevin"),
+                    ];
+
+
+//dbg(listeBuvettes[1].nom)
+
+/*Création du constructeur Matchs*/
+function Match (date, teamA, teamB, score, buvette) {
+    this.date = date;
+    this.teamA = teamA;
+    this.teamB = teamB;
+    this.score = score;
+    this.buvette = buvette;
+}
+
+var listeMatch = [new Match("01-01-2021", listeEquipe[0], listeEquipe[8], "13-0", listeBuvettes[1]),
+                  new Match("02-01-2021", listeEquipe[1], listeEquipe[9], "0-0", listeBuvettes[1]),
+                  new Match("03-01-2021", listeEquipe[2], listeEquipe[10], "2-1", listeBuvettes[2]),
+                  new Match("04-01-2021", listeEquipe[3], listeEquipe[11], "3-3", listeBuvettes[3]),
+                  new Match("05-01-2021", listeEquipe[4], listeEquipe[12], "5-2", listeBuvettes[4]),
+                  new Match("06-01-2021", listeEquipe[5], listeEquipe[13], "4-0", listeBuvettes[5]),
+                  new Match("07-01-2021", listeEquipe[6], listeEquipe[14], "1-3", listeBuvettes[6]),
+                  new Match("08-01-2021", listeEquipe[7], listeEquipe[15], "0-1", listeBuvettes[7])];
